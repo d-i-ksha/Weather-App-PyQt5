b7b035a6cc06416317a60fbdd20654c4bffedf35
d-i-ksha <dikshasaklani097@gmail.com>
2026-08-05 10:04:51 +0530

    Temperature in Celsius with description and emoji added

--- main.py
+++ main.py
@@ -122,9 +122,41 @@ class WeatherApp(QWidget):
         self.temperature_label.setText(message)
 
     def display_weather(self,data):
-        print(data)
+        self.temperature_label.setStyleSheet("font-size:75px")
+        temperature_k=data["main"]["temp"]
+        temperature_c=temperature_k-273.15
+        temperature_f=(temperature_k*9/5)-459.67
+        weather_id=data["weather"][0]["id"]
+        weather_desc=data["weather"][0]["description"]
+        self.temperature_label.setText(f"{temperature_c:.0f} °C")
+        self.emoji_label.setText(self.get_weather_emoji(weather_id))
+        self.description_label.setText(weather_desc)
 
-
+    @staticmethod
+    def get_weather_emoji(weather_id):
+        if 200 <=weather_id<=232:
+            return "⛈️"
+        elif 300<=weather_id<=321:
+            return "🌤️"
+        elif 500<=weather_id<=531:
+            return "🌧️"
+        elif 600<=weather_id<=622:
+            return "🌨️"
+        elif 701<=weather_id<=741:
+            return "🌫️"
+        elif weather_id==762:
+            return "🌋"
+        elif weather_id==771:
+            return "💨"
+        elif weather_id==781:
+            return "🌪️"
+        elif weather_id==800:
+            return "🌞"
+        elif 801<=weather_id<=804:
+            return "☁️"
+        else:
+            return ""
+        
 if __name__=='__main__':
     app=QApplication(sys.argv)
     weather_app=WeatherApp()
